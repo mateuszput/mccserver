476875d73c2496dd0b1a88410ef5b3feb4270539
Mateusz Put <mateusz.put@gmail.com>
2017-05-30 22:27:40 +0200

    laczenie kodu workera z serwerem

--- src/main/java/net/mcc/wrappers/VMConnector.java
+++ src/main/java/net/mcc/wrappers/VMConnector.java
@@ -15,17 +15,18 @@ public class VMConnector {
     public void startTask(String server, Long taskID) throws IOException {
         StringBuffer body = new StringBuffer();
         body.append("{\"taskID\": \"").append(taskID).append("\"");
+        String path = "/startTask/" + taskID.toString();
 
-        HttpURLConnection httpURLConnection = createPOSTURL(server, taskID, body.toString());
+        HttpURLConnection httpURLConnection = createPOSTURL(server, taskID, body.toString(), path);
         int responseCode = httpURLConnection.getResponseCode();
 
         // Note: Czytanie odpowiedzi - nie potrzebne raczej - tylko response code!
 //        StringBuffer response = readResponse(httpURLConnection);
     }
 
-    private HttpURLConnection createPOSTURL(String server, Long taskID, String postBody) throws IOException {
+    private HttpURLConnection createPOSTURL(String server, Long taskID, String postBody, String path) throws IOException {
         StringBuffer urlAddress = new StringBuffer(server);
-        urlAddress.append("");
+        urlAddress.append(path);
 
         URL url = new URL(urlAddress.toString());
         HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
